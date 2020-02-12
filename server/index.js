651ce4fa0cdcda6d63211cceeb787387012eea99
Konstantin Gulyy <kgulyy@my.com>
2020-02-13 02:58:35 +0300

    Add logging

--- server/index.js
+++ server/index.js
@@ -2,20 +2,24 @@
 
 const http = require('http');
 const fs = require('fs');
+const debug = require('debug');
+
+const log = debug('*');
 
 const server = http.createServer(function (req, res) {
-    console.log(req.method, req.url);
+    log(req.method, req.url);
     const filename = req.url === '/' ? req.url + 'index.html' : req.url;
 
     fs.readFile(`public/${filename}`, (err, file) => {
         if (err) {
-            console.log('Error: ', err);
+            log('Error: ', err);
             res.writeHead(404);
             res.write('Page not found: 404');
             res.end();
             return;
         }
 
+        log('OK: ', filename);
         res.write(file);
         res.end();
     });
